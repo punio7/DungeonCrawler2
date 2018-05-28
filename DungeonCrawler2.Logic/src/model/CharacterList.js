d2c299bdc19a61522f1eed642b6ccbecb2d46928
Przemysław Mućka <przemyslaw.mucka@pentacomp.pl>
2018-05-28 16:17:30 +0200

    Sprawdzanie typu przy dodawaniu obiektów do list

--- DungeonCrawler2.Logic/src/model/CharacterList.js
+++ DungeonCrawler2.Logic/src/model/CharacterList.js
@@ -3,4 +3,11 @@ class CharacterList extends EntityList {
     constructor() {
         super();
     }
+
+    add(character) {
+        if (!character instanceof Character) {
+            throw new "Only Characters can be added to character list";
+        }
+        super.add(character);
+    }
 }
--- DungeonCrawler2.Logic/src/model/ItemList.js
+++ DungeonCrawler2.Logic/src/model/ItemList.js
@@ -3,7 +3,14 @@ class ItemList extends EntityList {
     constructor() {
         super();
     }
-    
+
+    add(item) {
+        if (!item instanceof Item) {
+            throw new "Only Items can be added to item list";
+        }
+        super.add(item);
+    }
+
     hasLightSource() {
         return this.Array.some(i => i.isLightSource() === true);
     }
